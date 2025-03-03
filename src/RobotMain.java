a29e6157c3327bcb3c4fa2c48cfce2edc710267b
Gaeun Yoo <gaeunyoo3626@gmail.com>
2025-03-03 12:10:38 -0500

    clean up

--- src/RobotMain.java
+++ src/RobotMain.java
@@ -1,12 +1,11 @@
 /**
- * project name: U1U3 Crazy Name V2
- * @author Sarah Yoo
- * completed date: 2025/ 2 / 20
+ * project name: U2A2
+ * @author Sarah Yoo, Jiamiao Zheng
+ * completed date: 2025 / 3 /
  */
 public class RobotMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		RobotTask task = new RobotTask();
 		task.run();
 	}
--- src/RobotTask.java
+++ src/RobotTask.java
@@ -8,14 +8,5 @@ public class RobotTask {
 	public void run() {
 		// build a random city called waterloo
 		City waterloo = new City(100, 100);
-		
-		// build a robot in the city waterloo starting at street 4, avenue 1
-		LetterBot r = new LetterBot(waterloo, 1, 1, 999);	
-		//r.setSpeed(60);
-		r.drawS();
-		r.drawA();
-		r.drawR();
-		r.drawA();
-		r.drawH();
 	}
 }
