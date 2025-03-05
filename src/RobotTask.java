1cb94b5f33b4c846cb74d3f766cb44159ded0319
Gaeun Yoo <gaeunyoo3626@gmail.com>
2025-03-05 12:20:44 -0500

    polymorphism and multithreading

--- src/RobotTask.java
+++ src/RobotTask.java
@@ -8,17 +8,34 @@ public class RobotTask {
 	 */
 	public void run() {
 		// build a random city called waterloo
-		City waterloo = new City(100, 100);
-		NumberBot r = new NumberBot(waterloo, 0, 0, Direction.NORTH, 999);
-
+		City field = new City(100, 100);
+		TimeBot r1 = new TimeBot(field, 0, 0);
+		TimeBot r2 = new TimeBot(field, 0, 7);
+		TimeBot r3 = new TimeBot(field, 0, 13);
+		TimeBot r4 = new TimeBot(field, 0, 20);
+		
+		//for format
+		new Thing(field, 2, 14);
+		new Thing(field, 4, 14);
+		
 		String time = LocalTime.now().toString();
 		System.err.println(time);
 
-		for (char c : time.toCharArray()) {
-			System.err.println(c);
-			if (Character.isDigit(c)) {
-				r.draw_digit(Character.getNumericValue(c));
-			} 
-		}
+		Thread thread1 = new Thread(() -> {
+			r1.draw_digit(Character.getNumericValue(time.charAt(0)));
+		});
+		Thread thread2 = new Thread(() -> {
+			r2.draw_digit(Character.getNumericValue(time.charAt(1)));
+		});
+		Thread thread3 = new Thread(() -> {
+			r3.draw_digit(Character.getNumericValue(time.charAt(3)));
+		});
+		Thread thread4 = new Thread(() -> {
+			r4.draw_digit(Character.getNumericValue(time.charAt(4)));
+		});
+		thread1.start();
+		thread2.start();
+		thread3.start();
+		thread4.start();
 	}
 }
--- src/TimeBot.java
+++ src/TimeBot.java
@@ -1,8 +1,38 @@
 import becker.robots.*;
+import java.util.HashMap;
 
-public abstract class TimeBot extends NumberBot{
+public class TimeBot extends NumberBot{
 
     public TimeBot(City city, int x, int y, Direction direction, int things) {
         super(city, x, y, direction, things);
+    }
+    
+    public TimeBot(City city, int x, int y) {
+        super(city, x, y, Direction.NORTH, 999);
+    }
+
+    public static final HashMap<Integer, Point[]> digitsmap = new HashMap<Integer, Point[]>() {{
+        put(0, new Point[]{new Point(4, 0), new Point(0, -6), new Point(-4, 0), new Point(0, 6), new Point(6, 0, false)});
+        put(1, new Point[]{new Point(4, 0, false), new Point(0, -7), new Point(2, 7, false)});
+        put(2, new Point[]{new Point(4, -3), new Point(-4, -3), new Point(5, 0), new Point(1, 6, false)});
+        put(3, new Point[]{new Point(4, -3), new Point(-5, 0), new Point(5, 0, false), new Point(0, -3), new Point(-5, 0), new Point(7, 6, false)});
+        put(4, new Point[]{new Point(0, -3), new Point(4, 4), new Point(0, -3, false), new Point(0, -5), new Point(2, 7, false)});
+        put(5, new Point[]{new Point(5, 0), new Point(-5, 0, false), new Point(0, -3), new Point(4, -3), new Point(-5, 0), new Point(7, 6, false)});
+        put(6, new Point[]{new Point(5, 0), new Point(-5, 0, false), new Point(0, -3), new Point(4, -3), new Point(-4, 3), new Point(6, 3, false)});
+        put(7, new Point[]{new Point(4, -6), new Point(2, 6, false)});
+        put(8, new Point[]{new Point(4, -6), new Point(-4, 6), new Point(0, -3, false), new Point(4, 0), new Point(2, 3, false)});
+        put(9, new Point[]{new Point(4, -6), new Point(-5, 0), new Point(1, 3, false), new Point(4, 0), new Point(-4, 0, false), new Point(0, 3), new Point(6, 0, false)});
+    }};
+
+    @Override
+    public void draw_digit(int digit) {
+        Point[] points = digitsmap.get(digit);
+        for (Point point : points) {
+            if (point.putThing) {
+                putThenMove(point);
+            } else {
+                move(point);
+            }
+        }
     }
 }
